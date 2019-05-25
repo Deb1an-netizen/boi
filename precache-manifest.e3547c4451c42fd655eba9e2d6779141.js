1b96a9132f94b0814390cbf4ea697e1f5326599d
Nawaz Sk <nawazmanu@gmail.com>
2019-05-26 02:46:06 +0530

    Updates

---
+++ precache-manifest.e3547c4451c42fd655eba9e2d6779141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131fd4eda7fbb45496b60faf995aad67",
+    "url": "/boi/index.html"
+  },
+  {
+    "revision": "180f7d4c873184d49b16",
+    "url": "/boi/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "8998a974c2aeab08dca4",
+    "url": "/boi/static/js/2.3de82fab.chunk.js"
+  },
+  {
+    "revision": "180f7d4c873184d49b16",
+    "url": "/boi/static/js/main.1a9e88ee.chunk.js"
+  },
+  {
+    "revision": "8ee886a1b6c40b6ab098",
+    "url": "/boi/static/js/runtime~main.d7a3d389.js"
+  }
+]);
